cd8c647e90acff7545bab375612691f7f8611e22
Marina Freitas <160733141+M4rinaVict0ria@users.noreply.github.com>
2025-11-26 09:30:58 +0000

    teste3

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,11 @@ import 'dotenv/config';
 
 export default {
   development: {
-    client: 'sqlite3', // driver do SQLite
-    connection: {
-      filename: path.resolve('db', 'dev.sqlite3') // caminho absoluto para o arquivo do banco
-    },
-    useNullAsDefault: true, // necessário para SQLite (evita warnings do Knex)
+    client: 'sqlite3',
+connection: {
+  filename: path.resolve('db', 'dev.sqlite3')
+},
+useNullAsDefault: true,
     migrations: {
       directory: path.resolve('db', 'migrations') // onde ficam os arquivos de migração
     }
